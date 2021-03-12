3808ef49d93ea44e969c3ddc1a59eb2fb9f7b660
JuanPablooo <juanpablopereiragomes@hotmail.com>
2021-03-12 20:32:17 -0300

    feature(setup) configurações e dependencias iniciais do projeto

---
+++ src/test/java/br/com/drive/DriveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.drive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
