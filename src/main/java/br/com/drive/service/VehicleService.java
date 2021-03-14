900a1b7df32587048d54fdddce4ed28308cf86e3
JuanPablooo <juanpablopereiragomes@hotmail.com>
2021-03-13 22:13:55 -0300

    feature(register) created register feature(register) created request-mapping to register vehicle, get vehicle price of kbb and another info

--- src/main/java/br/com/drive/service/VehicleService.java
+++ src/main/java/br/com/drive/service/VehicleService.java
@@ -35,6 +35,46 @@ public class VehicleService {
     private final BrandService brandService;
     private final KbbRestClient kbbClient;
 
+    public List<Vehicle> getAllVehicles(){
+        return vehicleRepository.findAll();
+    }
+
+    public Vehicle create(VehicleRequest vehicleRequest) {
+        ModelYear modelYearFound = getModelYearByVehicleRequest(vehicleRequest);
+        Optional<KbbPrice> optKbbPrice = kbbClient.getOne(modelYearFound.getKbbId()).getBody();
+        KbbPrice kbbPriceFound = optKbbPrice.orElseThrow(()->new IllegalArgumentException("price kbb is not found"));
+        Vehicle vehicleToBeSaved = Vehicle.builder()
+                .modelYear(modelYearFound)
+                .kbbPrice(kbbPriceFound.getPrice())
+                .licensePlate(vehicleRequest.getPlaca())
+                .adPrice(vehicleRequest.getPrecoAnuncio())
+                .build();
+        return vehicleRepository.save(vehicleToBeSaved);
+    }
+
+    // this method return detail error, but is not the most better on performer
+    private ModelYear getModelYearByVehicleRequest(VehicleRequest vehicleRequest){
+        log.info("find model by id {}", vehicleRequest.getModeloId());
+        Model modelFound = modelService.getOneOrThrow(vehicleRequest.getModeloId());
+        log.info("find brand by id {}", vehicleRequest.getMarcaId());
+        Brand brandFound = brandService.getOneOrThrow(vehicleRequest.getMarcaId());
+        log.info("find modelYear");
+        Optional<ModelYear> optModelYear = modelYearService
+                .getOneByYearAndModelAndBrand( vehicleRequest.getAno(), modelFound, brandFound) ;
+        if( optModelYear.isPresent() ) return optModelYear.get();
+        log.info("modelYear not found by year {} or brand not contain model", vehicleRequest.getAno());
+        throw new IllegalArgumentException("ano para este modelo e está marca não etá disponivél");
+    }
+
+    // this is faster but user not receive detail message response when not found
+    private ModelYear getModelYearByVehicleRequestFaster(VehicleRequest vehicleRequest){
+        Integer year = vehicleRequest.getAno();
+        UUID modelId = vehicleRequest.getModeloId();
+        UUID brandId = vehicleRequest.getMarcaId();
+        Optional<ModelYear> optModelYearFound = modelYearService.getOneByYearAndModelIdAndBrandId(year, modelId, brandId);
+        if( optModelYearFound.isPresent() ) return optModelYearFound.get();
+        throw new IllegalArgumentException("os dados do veículo não são validos");
+    }
 
     public Vehicle getOneOrThrow(UUID uuid){
         return vehicleRepository.findById(uuid)
