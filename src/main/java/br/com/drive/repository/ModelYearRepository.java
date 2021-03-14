03859008571dc87e11da1a3982d4b6b1ce7dd575
JuanPablooo <juanpablopereiragomes@hotmail.com>
2021-03-13 21:52:18 -0300

    feature(domain) create custom method to filter modelYear by year, modelId and BrandId

--- src/main/java/br/com/drive/repository/ModelYearRepository.java
+++ src/main/java/br/com/drive/repository/ModelYearRepository.java
@@ -2,11 +2,16 @@ package br.com.drive.repository;
 
 import br.com.drive.entity.ModelYear;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ModelYearRepository extends JpaRepository<ModelYear, UUID> {
-
+    @Query(value =
+            "SELECT md FROM ModelYear md " +
+                    "WHERE md.year = :year AND md.model.id = :modelId AND md.model.brand.id = :brandId ")
+    Optional<ModelYear> findByYearAndModelIdAndBrandId(Integer year, UUID modelId, UUID brandId);
 }
